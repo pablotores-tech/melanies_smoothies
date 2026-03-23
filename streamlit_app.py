a62dd491e194b3016a3940a4f3b136bda1c15336
pablotores-tech <pablo.tores@sdggroup.com>
2026-03-23 10:47:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if ingredient_list:
     ingredient_string = ''
     for f in ingredient_list:
         ingredient_string += f + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st.subheader(fruit_chosen+' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
