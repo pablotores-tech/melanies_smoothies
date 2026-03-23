98d22e454dbf227c811cad2e54c66c5fea1a1fdc
pablotores-tech <pablo.tores@sdggroup.com>
2026-03-23 11:02:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 name = st.text_input("Smooothie name")
 st.write("The name on your smoothie will be", name)
 
