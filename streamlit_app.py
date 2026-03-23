5adebd270be188844d6b2ad6d4faaf7970aada43
pablotores-tech <pablo.tores@sdggroup.com>
2026-03-23 10:46:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if ingredient_list:
     ingredient_string = ''
     for f in ingredient_list:
         ingredient_string += f + ' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                         values ('""" + ingredient_string + """','"""+name+"""')"""
@@ -39,7 +40,4 @@ if ingredient_list:
     if confirm:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+  
